ae4b6a084d4d3618636dff3089055e3ea0ba9d96
Firtina Ozbalikci <firtina14@gmail.com>
2014-02-04 18:35:14 +0000

    Moving scripts out of Editor directories

--- EditorHelpers.cs
+++ EditorHelpers.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System;
 using toxicFork.GUIHelpers.DisposableGUI;
 using toxicFork.GUIHelpers.DisposableHandles;
@@ -169,3 +170,5 @@ namespace toxicFork.GUIHelpers {
 		}
 	}
 }
+
+#endif
