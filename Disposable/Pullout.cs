0f1684aba12d00e838f2e567a85a669ba58c71cb
Firtina Ozbalikci <firtina14@gmail.com>
2014-06-01 20:28:18 +0100

    Fixed building

--- Disposable/Pullout.cs
+++ Disposable/Pullout.cs
@@ -1,8 +1,8 @@
+#if UNITY_EDITOR
 using toxicFork.GUIHelpers.DisposableEditorGUILayout;
 using toxicFork.GUIHelpers.DisposableGUI;
 using toxicFork.GUIHelpers.DisposableGUILayout;
 using toxicFork.GUIHelpers.DisposableHandles;
-#if UNITY_EDITOR
 using System;
 using UnityEditor;
 using UnityEngine;
